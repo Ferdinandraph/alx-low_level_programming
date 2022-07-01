0d2308bbb31ac453ca78c6a191b121eda8555f0b
root <Ferdinand raphael>
2022-07-01 00:49:49 -0700

    fixed bug

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+/**
+ * main- printing the size of types
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s) \n", sizeof(c));
+	printf("Size of a int: %d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %d byte(s) \n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %d byte(s) \n", sizeof(float));
+	return (0);
+}
