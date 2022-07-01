ed9b3f48db0a4fa8ada43a3c1d283309669a7c5c
root <Ferdinand raphael>
2022-07-01 13:43:08 -0700

    file commited

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing all digit number from base 10
+ * Return 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf(x"\n");
+	}
+	return (0);
+}
