80944550cc6b98239bc341b5ccbf79c960ce7563
root <Ferdinand raphael>
2022-07-01 16:07:12 -0700

    task completed

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all number of base 10
+ * Return: 0
+ */
+ int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
