021200212b4e3b205b7504106fc5c809e64ae87b
root <Ferdinand raphael>
2022-07-01 14:52:41 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (x = 'a'; x <= 'z'; ++x)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
