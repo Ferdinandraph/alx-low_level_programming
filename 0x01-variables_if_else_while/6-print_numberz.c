8112bab3b6058b9707f6071eae661cc698ac2d6c
root <Ferdinand raphael>
2022-07-01 14:33:08 -0700

    6 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <studio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing digit number base 10 without printf e t c
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
