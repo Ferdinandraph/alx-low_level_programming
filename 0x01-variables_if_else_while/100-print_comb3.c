86117362e0c77d11610664b981d245a6d343dc1b
root <Ferdinand raphael>
2022-07-01 16:23:39 -0700

    file commited

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int w, y;
+
+	for (w = '0'; w <= '9'; w++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(w);
+			putchar(y);
+			if (w == '9' && y == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
