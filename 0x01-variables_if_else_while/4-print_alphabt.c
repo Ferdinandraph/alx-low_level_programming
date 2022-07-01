f35f97f0f80cd82a6cebc8cbda79b143b1fbac7e
root <Ferdinand raphael>
2022-07-01 13:12:36 -0700

    file commited

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing lowercase alphabet a to z without q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if ((x != 'q') || (x != 'e'))
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
