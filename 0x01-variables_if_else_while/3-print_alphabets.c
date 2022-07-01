e4c5ab05550cdff704e18c739bf12d21ba487aee
root <Ferdinand raphael>
2022-07-01 12:52:17 -0700

    file commited

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: printing lowercase and uppercase alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x == 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	for (y == 'A'; y <= 'Z'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
