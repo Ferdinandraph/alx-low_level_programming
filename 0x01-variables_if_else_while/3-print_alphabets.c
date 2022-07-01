6f17542c028819eece559b2f8c0f860e740aefd8
root <Ferdinand raphael>
2022-07-01 12:55:59 -0700

    fixed bug

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int x, y;
 
-	for (x == 'a'; x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
-	for (y == 'A'; y <= 'Z'; y++)
+	for (y = 'A'; y <= 'Z'; y++)
 	{
 		putchar(y);
 	}
