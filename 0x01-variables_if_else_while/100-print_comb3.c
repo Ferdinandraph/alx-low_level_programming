a71dc79b580f03ff01fc7881ee422ed06ca4b89d
root <Ferdinand raphael>
2022-07-01 16:40:17 -0700

    advanced task added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 	int w, y;
 
-	for (w = '0'; w <= '9'; w++)
+	for (w = 48; w <= 58; w++)
 	{
-		for (y = '0'; y <= '9'; y++)
+		for (y = 48; y <= 58; y++)
 		{
 			putchar(w);
 			putchar(y);
-			if (w == '9' && y == '9')
+			if (w == 57 && y == 57)
 			{
 				break;
 			}
