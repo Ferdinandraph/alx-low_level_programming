9af2ead54116954686e618fc26aff93cd39b4208
root <Ferdinand raphael>
2022-07-05 08:01:08 -0700

    add task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing a-z *10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+
+	while (counter <= 10)
+	{
+		char x = 'a';
+
+		while (x <= 'z')
+		{
+			_putchar('x');
+			x++;
+		}
+		counter++;
+		_putchar('\n');
+	}
+}
