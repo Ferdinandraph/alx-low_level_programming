2de79919ba4a277d765e1a1b1bca09c6372160ee
root <Ferdinand raphael>
2022-07-06 05:15:54 -0700

    correction

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,20 +5,20 @@
  * @n: first parameter
  * Return: r
  */
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	int r;
 
 	if (n < 0)
 	{
 		r = -1 * (n % 10);
-		_putchar(r + '0')
+		_putchar(r + '0');
 		return (r);
 	}
 	else
 	{
 		r = n % 10;
 		_putchar(r + '0');
-		return (0);
+		return (r);
 	}
 }
