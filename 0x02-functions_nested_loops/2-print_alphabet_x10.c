cd2e558d2169ef3fce92331f1b62616d7f58d12c
root <Ferdinand raphael>
2022-07-06 00:30:29 -0700

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,15 @@
-#include "stdio.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
 /**
  * print_alphabet_x10 - printing a-z *10
  *
@@ -21,14 +32,4 @@ void print_alphabet_x10(void)
 		putchar('\n');
 	}
 }
-void print_alphabet_x10(void);
-/**
- * main - entry point
- *
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+
