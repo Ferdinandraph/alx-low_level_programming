e5a68a42ecf91c153bdbde1fe05ae88fe9869d25
root <Ferdinand raphael>
2022-07-06 01:04:47 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,15 @@
-#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 /**
  * print_alphabet - print lowercase a-z
  *
