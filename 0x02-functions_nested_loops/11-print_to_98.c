1955d0b1fb8fb2bc907917dfb44b009f9afb8798
root <Ferdinand raphael>
2022-07-06 10:19:46 -0700

    last madatory task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print natural numbers from 98
+ *
+ * @n: first parameter
+ * Return: n
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		printf("%d, " n);
+	}
+}
