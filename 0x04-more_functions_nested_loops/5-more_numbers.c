0582bd7dce6d57d17874b11870a0feb124c974c1
root <Ferdinand raphael>
2022-07-07 23:39:48 -0700

    added file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y >= 10)
-				_putchar(y / 10 + '0');
-
+			if (y >= 9)
+			_putchar(y / 10 + '0');
+			else
 				_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
