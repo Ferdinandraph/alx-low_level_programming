8b85fd0b58c5b3c3e92ab90d3217d1a6af3f8bf5
root <Ferdinand raphael>
2022-07-08 01:28:36 -0700

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,14 +8,13 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x <= 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
-			if (y >= 9)
-			_putchar(y / 10 + '0');
-			else
-				_putchar(y % 10 + '0');
+			if (y >= 10)
+				_putchar((y / 10) + 48);
+			_putchar((y % 10) + 48);
 		}
 		_putchar('\n');
 	}
