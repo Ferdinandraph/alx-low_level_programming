4345149cf7db0211725ba1238772a92a9ed4f716
root <Ferdinand raphael>
2022-07-14 08:29:04 -0700

    file added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - comparing two strings
+ *
+ * @s1: first pointer parameter
+ * @s2: second pointer parameter
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int flag = 0;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0)
+		return (0);
+	else
+		return (1);
+
+}
