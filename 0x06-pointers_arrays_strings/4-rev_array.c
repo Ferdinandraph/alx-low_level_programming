d9f811d7cd6e7f5ea1de3a3996db84d7bb9043b8
root <Ferdinand raphael>
2022-07-14 20:14:37 -0700

    file added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reversing of array
+ *
+ * @a: pointer parameter
+ * @n: last parameter
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int ch;
+
+	for (i = 0; i < n/2; i++)
+	{
+		ch = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = ch
+	}
+}
