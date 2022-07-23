55f91bbdde95b6933aded75d08d923dd23a0ad2e
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-07-23 09:30:37 -0700

    file added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - printing the number passed to the argument
+ * @argc: counter arg
+ * @argv: pointer string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
