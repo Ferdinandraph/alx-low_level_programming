7bb05328d25f34aede9c359745c5c6e75d3a7889
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-07-23 11:06:13 -0700

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,14 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	int sum = 0;
 
 	for (i = 0; i < argc; i++)
 	{
-		if (*argv[i] != '\0')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
