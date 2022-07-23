bf957e1f4f02fb8d0af97e0ecbab43abd3ca0a45
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-07-23 09:47:42 -0700

    file added

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - printing a arguments
+ * @argc: counter arg
+ * @argv: pointer string
+ * return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		if (count >= 0)
+		{
+			printf("%s\n", argv[count]);
+		}
+	}
+	return (0);
+}
