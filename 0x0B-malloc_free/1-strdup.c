d2b78969870d30668cd00073ee686dc2d574b90a
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-07-26 03:37:12 -0700

    file added

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - allocate space memory
+ * @str: pointer character
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *duplicate_num;
+	int i = 0, length;
+
+	if (str == NULL)
+		return (NULL);
+	duplicate_num = malloc(sizeof(char) * length + 1);
+	for (; str[i]; i++)
+	{
+		duplicate_num[i] = str[i];
+		length++;
+	}
+	duplicate_num[length] = '\0';
+	return (duplicate_num);
+}
