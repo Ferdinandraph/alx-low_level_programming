bb50fcfe880aa328b9821285f15427814c814329
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-07-26 04:13:31 -0700

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,12 +11,14 @@ char *_strdup(char *str)
 	char *duplicate_num;
 	unsigned int i = 0, length = 0;
 
+	if (str == NULL)
+		return (NULL);
 	for (length = 0; str[length] != '\0'; length++)
 		;
 	duplicate_num = (char *)malloc(sizeof(char) * (length + 1));
-	if (str == NULL || duplicate_num == NULL)
+	if (duplicate_num == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; i <= length; i++)
 	{
 		duplicate_num[i] = str[i];
 	}
