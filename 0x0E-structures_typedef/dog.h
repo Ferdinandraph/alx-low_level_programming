d27b4c343919649a85f5e2ccaa289f658606dc11
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-02 03:36:30 -0700

    first file added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct dog
+ * @name: first object pointer for name
+ * @age: second object for age
+ * @owner: last object for owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
