e5e2984ae2b40300214c459b39ab88175a4717e9
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-15 12:20:25 -0700

    files added

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,11 +2,12 @@
 /**
  * print_listint - a function that prints all the elements of a listint_t list
  * @h: head
+ * Return: count
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
