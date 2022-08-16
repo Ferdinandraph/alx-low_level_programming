0ec0ae076a9537a798b6ab308edce06c010a94e4
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-16 07:42:08 -0700

    file added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists"
+/**
+ * pop_listint - function that deletes a head node and returns data
+ * @head: head
+ * Return: data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *h;
+	listint_t *curr;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+	curr = *head;
+	data = curr->n;
+	h = curr->next;
+	free(curr);
+	*head = h;
+	return (data);
+}
