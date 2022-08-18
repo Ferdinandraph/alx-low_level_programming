7ddbccdf832f7f233df00bd151a266dc9951265c
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-18 07:57:29 -0700

    file added

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary numbers to a string
+ * @b: pointer to string a and b
+ * Return: integer value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dex_val = 0;
+	int len = 0;
+
+	if (b[len] == '\0')
+		return (0);
+
+	for (; b[len]; len++)
+	{
+		if (b[len] == '1' || b[len] == '0')
+		{
+			dex_val = dex_val << 1;
+			dex_val = dex_val + b[len] - '0';
+		}
+	}
+	return (dex_val);
+}
