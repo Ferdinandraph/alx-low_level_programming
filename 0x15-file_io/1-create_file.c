0413eb6ce67b2991dd135ffdb90630d8947d6474
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-23 04:38:54 -0700

    file added

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file -  a function that creates a file
+ * @filename: file name
+ * @text_content: content
+ * Return: int
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nl;
+	int rw;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+		text_content = " ";
+	for (nl = 0; text_content[nl]; nl++)
+		;
+	rw = write(fd, nl, text_content);
+	if (rw == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
