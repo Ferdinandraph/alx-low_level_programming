0710d73b7ae0eb2b290da5bd7d07f44dbb46c053
Ferdinandraph <ferdinandraphael20@gmail.com>
2022-08-23 04:42:46 -0700

    file added

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	for (nl = 0; text_content[nl]; nl++)
 		;
-	rw = write(fd, nl, text_content);
+	rw = write(fd, text_content, nl);
 	if (rw == -1)
 		return (-1);
 	close(fd);
